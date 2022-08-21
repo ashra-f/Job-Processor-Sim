02691cca35736872dc130d210d11df04ce9c572e
Ash H <ashasaurus@protonmail.com>
2022-08-21 15:29:45 -0400

    Changed header radius. Added input button to capture number of processors.

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ function closeMod(modal) {
 }
 
 document.getElementById("generate_jobs_btn").onclick = function() {
+    document.getElementById("num_of_processors").style.visibility = "visible";
+
     let all_jobs = createJobs();
     mergeSort(all_jobs, 0, all_jobs.length - 1);
     addJobs(all_jobs);
