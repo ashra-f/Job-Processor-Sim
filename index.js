8a6f960decbace75148a08b577ea65e59dd5de20
Ash H <ashasaurus@protonmail.com>
2022-08-21 16:30:20 -0400

    create processors vector

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ document.getElementById("generate_jobs_btn").onclick = function() {
     document.getElementById("show_data_btn").style.visibility = "visible";
     document.getElementById("generate_jobs_btn").disabled = true;
 }
+
+document.getElementById("process_count_btn").onclick = function() {
+    num_processors = document.getElementById("process_count").value;
+    document.getElementById("process_count_btn").disabled = true;
+
+    // create n number of processors --> add into vector
+    let processors = new Array(num_processors);
+    for (let i = 0; i < num_processors; i++) {
+        processors[i] = new Processor;
+    }
+}
